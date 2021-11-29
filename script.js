c0f6469cba9a0579c7942947d065844b39cee482
Josue Ramirez-Romero <josueramirez35827@gmail.com>
2021-11-29 00:39:56 -0500

    homework

--- script.js
+++ script.js
@@ -7,13 +7,12 @@ let quotes = [
   `It don't matter if you win by an inch or a mile. Winning's winning.`,
 ];
 
-window.onload =  function(event) {
-  
+window.onload = function (event) {
+
   // Random quote of the day generator
-  const randomQuote = function() {
-    document.querySelector('#quote-of-the-day').textContent = `"${
-      quotes[Math.floor(Math.random() * quotes.length)]
-    }"`;
+  const randomQuote = function () {
+    document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]
+      }"`;
   };
   randomQuote();
 
@@ -21,15 +20,53 @@ window.onload =  function(event) {
 
   // Part 1
 
+  document.getElementById("main-title").textContent = "Welcome to Dom's Homepage!";
+
   // Part 2
+
+  document.body.style.background = "lightblue";
 
   // Part 3
 
+  let Staring = document.getElementById("favorite-things").children[5];
+  Staring.remove();
+
   // Part 4
+
+  let elements = document.getElementsByClassName("special-title");
+
+  for (i = 0; i < elements.length; i++) {
+    let element = elements[i];
+    element.style.fontSize = "2rem";
+  }
 
   // Part 5
 
+  let Chicago = document.getElementById("past-races").children[3];
+  Chicago.remove();
+
+
   // Part 6
 
+  let node = document.createElement("li");
+  node.innerHTML = "New York";
+
+  document.getElementById("past-races").appendChild(node);
+
   // Part 7
+
+  let mainDiv = document.querySelector(".main");
+  let newDiv = document.createElement("div");
+
+  mainDiv.appendChild(newDiv);
+  newDiv.className = "blog-post purple test";
+
+  let h1 = document.createElement("h1");
+  h1.innerText = "New York";
+
+  let p1 = document.createElement("p");
+  p1.innerText = "THE CONRETE JUNGLE HAS NOTHING ON THE DOM!";
+
+  newDiv.append(h1, p1);
+
 }
